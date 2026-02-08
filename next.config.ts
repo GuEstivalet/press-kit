6424401ec85988e54e450092c60b478c8bbce465
Gustavo Estivalet <guestivalet@hotmail.com>
2026-02-08 18:34:35 -0300

    att

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export'
+// next.config.ts
+import type { NextConfig } from "next";
+
+const repo = "press-kit"; // <- NOME DO REPO no GitHub
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
+const nextConfig: NextConfig = {
+  output: "export",          // static export (necessário para GitHub Pages)
+  trailingSlash: true,       // ajuda no roteamento estático
+  images: { unoptimized: true }, // evita otimização do next/image (não rola no Pages)
+
+  ...(isGithubPages
+    ? {
+        basePath: `/${repo}`,
+        assetPrefix: `/${repo}/`,
+      }
+    : {}),
 };
 
 export default nextConfig;
