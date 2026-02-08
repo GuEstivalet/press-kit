fd5491c256e3d903faded16a27fe25414aeabe9a
Gustavo Estivalet <guestivalet@hotmail.com>
2026-02-08 16:30:58 -0300

    atualizacao

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Ativa a exportação estática
+  output: 'export',
+  basePath: '/press-kit', // Nome exato do seu repositório
   images: {
-    unoptimized: true, // Necessário porque o GitHub Pages não suporta otimização de imagem nativa do Next
+    unoptimized: true,
   },
-  // Se o seu repositório NÃO for o principal (ex: username.github.io/meu-repositorio), 
-  // adicione a linha abaixo com o nome do repositório:
-  // basePath: '/nome-do-repositorio', 
 };
 
 export default nextConfig;
